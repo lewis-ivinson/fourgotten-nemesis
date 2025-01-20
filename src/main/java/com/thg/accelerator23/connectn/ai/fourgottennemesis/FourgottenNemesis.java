83e7e3b639e573fbc729009725dde14c3319956a
Lewis Ivinson <lewis.ivinson@thehutgroup.com>
2025-01-20 11:21:49 +0000

    Update names

--- src/main/java/com/thg/accelerator23/connectn/ai/fourgottennemesis/FourgottenNemesis.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/fourgottennemesis/FourgottenNemesis.java
@@ -1,20 +1,19 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.fourgottennemesis;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
-    //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+public class FourgottenNemesis extends Player {
+  public FourgottenNemesis(Counter counter) {
+    super(counter, FourgottenNemesis.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
+    return 1;
   }
 }
